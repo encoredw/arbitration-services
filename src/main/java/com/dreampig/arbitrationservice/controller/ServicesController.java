791cc21d144e103cdcaa5992f419852aa4069558
wangzl8@lenovo.com <wangzl8@lenovo.com>
2018-02-12 00:34:17 +0800

    增加放款流水请求

--- src/main/java/com/dreampig/arbitrationservice/controller/ServicesController.java
+++ src/main/java/com/dreampig/arbitrationservice/controller/ServicesController.java
@@ -2,6 +2,9 @@ package com.dreampig.arbitrationservice.controller;
 
 import com.dreampig.arbitrationservice.model.Authentication;
 import com.dreampig.arbitrationservice.service.AuthenticationService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,5 +25,35 @@ public class ServicesController {
         authentication.setPassword(password);
        return authenticationService.selectByAuthentication(authentication);
 
+    }
+    
+    @RequestMapping(value = "/applyStatementInitData",method = RequestMethod.POST)
+    @ResponseBody
+    public String applyStatementInitData(String type, String sid, String password,@RequestBody String requestBody){
+    	System.out.println(type+":"+sid+":"+password);
+        Authentication authentication = new Authentication();
+        authentication.setSid(sid);
+        authentication.setType(type);
+        authentication.setPassword(password);
+        authentication= authenticationService.selectByAuthentication(authentication);
+        
+        if(authentication!=null) {
+        	
+        	try {
+        		JSONObject applyStatementInitData=new JSONObject(requestBody);
+				applyStatementInitData.get("businessCode01");
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+    	return "";
+    }
+    
+    @RequestMapping(value = "/applyRepaymentInitData",method = RequestMethod.POST)
+    @ResponseBody
+    public String applyRepaymentInitData(String type, String sid, String password,@RequestBody String requestBody){
+       
+    	return "";
     }
 }
