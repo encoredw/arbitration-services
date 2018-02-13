b55e9235aab58f17012d3171eea6204dbd8ad227
chinabeijingchen <cn_cyc@163.com>
2018-02-13 17:04:25 +0800

    增加swagger-ui

---
+++ src/main/java/com/dreampig/arbitrationservice/config/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.dreampig.arbitrationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by houzhipeng on 2016/11/29.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dreampig.arbitrationservice.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("项目接口RESTful APIs")
+                .description("项目接口")
+                .termsOfServiceUrl("")
+                .version("1.0")
+                .build();
+    }
+}
